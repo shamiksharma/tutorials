23aec0618fb754e251fc453048a3acc56b2cf1a4
Shamik Sharma <givity@gmail.com>
2012-08-12 11:31:30 -0700

    yahoo and google both working with new oauth lib

--- oauth_samples/yahoo/settings.py
+++ oauth_samples/yahoo/settings.py
@@ -1,9 +1,18 @@
 
 YAHOO_CONSUMER_KEY="Fill This in"
 YAHOO_CONSUMER_SECRET="Fill This in"
+GMAIL_ADDRESS='your email address'
+
+
+GOOGLE_CONSUMER_KEY="anonymous"
+GOOGLE_CONSUMER_SECRET="anonymous"
+
+token_file_path = "/tmp/ytokens.txt"
+
+GOOGLE_SCOPE="https://mail.google.com/,https://www.googleapis.com/auth/userinfo.email,https://www.googleapis.com/auth/userinfo.profile,https://www.googleapis.com/auth/userinfo.id"
 
 try:
-    from local_settings.py import *  # override consumer_key, secret etc.
+   from local_settings import *  # override consumer_key, secret etc.
 except ImportError:
-    print "WARNING: No local_settings"
-    pass
+   print "WARNING: No local_settings"
+   pass
--- oauth_samples/yahoo/yahoo_test.py
+++ oauth_samples/yahoo/yahoo_test.py
@@ -1,22 +1,59 @@
 import settings
-from oauthmail.oauth import Oauth
+from oauthmail.oauth import Oauth, OauthSession
 from oauthmail.yahoo import YahooMail
+import sys, traceback
+import pickle
+import os
+import webbrowser
 
+
+#-----------------------------------
 
 class MyYahooMail(YahooMail):
     def access_persist(self, old_key, key, secret, handle):
         # Code for persisting Yahoo access token to your database
         return
 
+#-----------------------------------
 
 
-yahoo_oauth = Oauth(settings.YAHOO_CONSUMER_KEY, settings.YAHOO_CONSUMER_SECRET)
-yahoo_mail = MyYahooMail(yahoo_oauth)
+def main(argv=None):
+  
+  if argv is None:  argv = sys.argv
+  cmd = argv[0]
 
-# Fetch identifiers of unread mail
-unread_yahoo = yahoo_mail.mids_unread('Inbox')
+  oauth = Oauth(settings.YAHOO_CONSUMER_KEY, settings.YAHOO_CONSUMER_SECRET)
+   
+  token_file_path = settings.token_file_path
+  have_token = False;
+  # Check if we already have a token saved in the filesystem
+  if not os.path.exists(token_file_path):
+    session = MyYahooMail(oauth, None, None, None)
+    verifier_url = session.authorize("oob")
+    user_code = punch_auth_code(verifier_url)
+    token = session.access(user_code)
+    pickle.dump(token, open(token_file_path,"wb"))
+  else:
+    print 'Found existing tokens'
 
-# Fetch all Yahoo messages and print subjects to screen
-for mid in unread_yahoo:
-    msg = yahoo_mail.fetch('Inbox', mid)
-    print msg['subject']
+  token = pickle.load( open(token_file_path,"rb") )
+  mail_session = MyYahooMail(oauth, token[0], token[1], token[2])
+
+  # Fetch identifiers of unread mail
+  unread_mids = mail_session.mids_unread('inbox')
+
+  # Fetch all Yahoo messages and print subjects to screen
+  for mid in unread_mids:
+      try:
+        msg = mail_session.fetch('inbox', mid)
+      except:
+        print "Exception in user code:"
+        traceback.print_exc(file=sys.stdout)
+        
+      print msg['subject']
+
+
+#-------------------------------------------
+
+if __name__ == "__main__":
+  sys.exit(main())
